1bd7c0ffcc9a97b28b85d138b04d7e65a9c30fa3
MarcoPicca <il96marco@gmail.com>
2023-11-10 12:12:01 +0100

    snack 1 completo

--- SNACK-1/js/script.js
+++ SNACK-1/js/script.js
@@ -2,9 +2,9 @@ let ageFirstPerson = prompt('inserire età del primo soggetto');
 let ageSecondPerson = prompt('inserire età del secondo soggetto');
 
 if (ageFirstPerson > ageSecondPerson) {
-    alert(ageFirstPerson);
-} if else (ageSecondPerson > ageFirstPerson) {
-    alert(ageSecondPerson);
+    console.log('la prima età è più grande');
+} else if (ageSecondPerson > ageFirstPerson) {
+    console.log('la seconda età è più grande');
 } else {
-    alert(ageFirstPerson, ageSecondPerson);
+    console.log('hanno la stessa età');
 };
