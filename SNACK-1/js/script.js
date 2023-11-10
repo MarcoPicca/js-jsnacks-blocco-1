259be8dafa7fc2280b5b569a8d06b162c4e73513
MarcoPicca <il96marco@gmail.com>
2023-11-10 11:59:43 +0100

    snack completo non funzionante

--- SNACK-1/js/script.js
+++ SNACK-1/js/script.js
@@ -2,5 +2,9 @@ let ageFirstPerson = prompt('inserire età del primo soggetto');
 let ageSecondPerson = prompt('inserire età del secondo soggetto');
 
 if (ageFirstPerson > ageSecondPerson) {
-    console.log(ageFirstPerson);
-}
+    alert(ageFirstPerson);
+} if else (ageSecondPerson > ageFirstPerson) {
+    alert(ageSecondPerson);
+} else {
+    alert(ageFirstPerson, ageSecondPerson);
+};
