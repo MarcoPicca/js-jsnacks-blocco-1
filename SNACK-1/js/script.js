dfec9f9c2e8cf07755c49e5797b4e41e41c32a60
MarcoPicca <il96marco@gmail.com>
2023-11-10 11:52:52 +0100

    prima condizione

---
+++ SNACK-1/js/script.js
@@ -0,0 +1,6 @@
+let ageFirstPerson = prompt('inserire età del primo soggetto');
+let ageSecondPerson = prompt('inserire età del secondo soggetto');
+
+if (ageFirstPerson > ageSecondPerson) {
+    console.log(ageFirstPerson);
+}
