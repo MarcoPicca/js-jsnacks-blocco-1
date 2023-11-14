bbe5fedf980f05ea25e1ff0c762e7b1898c3c242
MarcoPicca <il96marco@gmail.com>
2023-11-14 12:18:58 +0100

    tentativo stampaggio numeri

---
+++ SNACK-2/js/script.js
@@ -0,0 +1,10 @@
+const h2 = document.getElementsByClassName('numeri');
+
+for (let i = 0; i < 5; i++){
+    if (i = 0, 2, 4){
+
+    } else if (i = 1, 3, 5){
+
+    }
+    document.getElementById(numeri).innerHTML = i;
+}
